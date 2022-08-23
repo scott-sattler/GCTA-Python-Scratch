fbbb8a70daf918ac4bf427cbe931f17cef12fc40
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-08-22 23:38:30 -0500

    updated; minor fixes; will start including tests at bottom

--- helper-functions.py
+++ helper-functions.py
@@ -1,10 +1,5 @@
-def id_data_type(id_this: any, data_type: any) -> str | tuple:
-    """
-
-    :param data_type:
-    :param id_this:
-    :return:
-    """
+# identify data type with its name as a string
+def id_data_type(id_this: any, data_type: str) -> bool | tuple:
     try:
         type_object = type(id_this)
     except Exception as exc:
@@ -16,3 +11,41 @@ def id_data_type(id_this: any, data_type: any) -> str | tuple:
 
 
 print("Helper functions successfully loaded!")
+
+
+# List comparison operators in a string, separated by spaces ('ComparisonOperators...')
+def comparison_operator_check(operators: str) -> bool:
+    operator_list = "== != > < >= <=".split(' ')
+    # remove spaces
+    usr_inp_no_spaces = [i for i in operators.split(' ') if i != '']
+    # check for uniqueness
+    usr_inp_hashed = {}
+    for each_operator in usr_inp_no_spaces:
+        usr_inp_hashed[each_operator] = ''
+
+    if len(usr_inp_no_spaces) != 6:
+        return False
+
+    for each_operator in operator_list:
+        if each_operator not in usr_inp_no_spaces:
+            return False
+
+    return True
+
+
+
+
+
+
+
+
+comparison_operators = " == != > < >= <= "
+comparison_operators = " !=  ==  >  <  >=  <=  "
+comparison_operators = " !=  ==     >  <  >=  < =  "
+comparison_operators = "  >   <  !=  ==    >=  < =  "
+comparison_operators = "  >   <  !=  ==    >=  <=  "
+
+
+# run the cell to check your answer
+x = comparison_operator_check(comparison_operators)
+print(x)
