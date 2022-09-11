5785a14243f60723b1116353a0e6a961849c62e8
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-09-10 22:18:23 -0500

    cleanup

--- helper-functions.py
+++ helper-functions.py
@@ -1,12 +1,20 @@
 # identify data type with its name as a string
+# ('BasicDataTypes...')
 def id_data_type(id_this: any, data_type: str) -> bool | tuple:
+    print("data_type", data_type, str(data_type))
+
     try:
         type_object = type(id_this)
     except Exception as exc:
         return exc.__class__.__name__, exc
 
+    print("type_object", type_object)
+
     exclusion_set = (' ', '\'', '\"', '\"\"\"', '<', '>')
     clean_id = ''.join([i for i in list(str(type_object).lstrip('<class ')) if i not in exclusion_set])
+
+    print(clean_id, data_type)
+
     return clean_id == data_type
 
 
@@ -36,13 +44,48 @@ print("Helper functions successfully loaded!")
 
 
 
-comparison_operators = " == != > < >= <= "
-comparison_operators = " !=  ==  >  <  >=  <=  "
-comparison_operators = " !=  ==     >  <  >=  < =  "
-comparison_operators = "  >   <  !=  ==    >=  < =  "
-comparison_operators = "  >   <  !=  ==    >=  <=  "
+# comparison_operators = " == != > < >= <= "
+# comparison_operators = " !=  ==  >  <  >=  <=  "
+# comparison_operators = " !=  ==     >  <  >=  < =  "
+# comparison_operators = "  >   <  !=  ==    >=  < =  "
+# comparison_operators = "  >   <  !=  ==    >=  <=  "
+#
+#
+# # run the cell to check your answer
+# x = comparison_operator_check(comparison_operators)
+# print(x)
+#
+#
+# # example problem
+#
+# # identify the data type of id_this
+# id_this = 1
+#
+# # assign your answer to data_type
+# # your answer will be in the form of a string
+# data_type = str
+#
+# print(data_type)
+# print(isinstance(data_type, str))
+#
+# # run the cell to check your answer
+# # print(id_data_type(id_this, data_type))
 
 
-# run the cell to check your answer
-x = comparison_operator_check(comparison_operators)
-print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
